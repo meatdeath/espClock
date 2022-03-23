4a3397888aff6c12ae8e7cdeb18e164e20004b30
Vladimir Novac <vladimir.novac.1980@gmail.com>
2022-03-23 17:34:58 -0400

    Add watch skeleton

--- data/script-dev.js
+++ data/script-dev.js
@@ -265,5 +265,20 @@ window.onload = function() {
     getFastTelemetry();
     setInterval( getPressureHistory, 5000 );
     updateTabs();
+
+    
+    setInterval(() => {
+        d = new Date(); //object of date()
+        hr = d.getHours();
+        min = d.getMinutes();
+        sec = d.getSeconds();
+        hr_rotation = 30 * hr + min / 2; //converting current time
+        min_rotation = 6 * min;
+        sec_rotation = 6 * sec;
+    
+        hour.style.transform = `rotate(${hr_rotation}deg)`;
+        minute.style.transform = `rotate(${min_rotation}deg)`;
+        second.style.transform = `rotate(${sec_rotation}deg)`;
+    }, 1000);
 }
 
